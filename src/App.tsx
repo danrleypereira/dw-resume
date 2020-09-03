595e2303e7231da58659e2e434a20f0d82b83465
danrleywillyan <danrleywillian@gmail.com>
2020-09-03 06:18:37 -0300

    resolving issue 'failed to compile' CI github

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './assets/logo.svg';
 import './App.css';
-
-import Navigation from './components/navigation/navigation';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
 import './about.css';
 
 import AsideLinks from '../../components/navigation/aside-links';
