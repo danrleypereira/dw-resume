0240ffb0cc7566d33c3e9ada7f7a940a1a0eee7d
danrleypereira <danrleywillian@gmail.com>
2022-04-05 03:26:12 -0300

    refactoring solution

--- src/components/navigation/aside-links.tsx
+++ src/components/navigation/aside-links.tsx
@@ -15,78 +15,119 @@ import instagram from '../../assets/social-midias/instagram.svg';
 
 import './navigation.css';
 
+const items = [
+    {
+        href: "https://github.com/danrleywillyan",
+        title: "home link",
+        icon: perfil,
+        alt: "",
+        text: "Danrley Pereira"
+    },
+    {
+        href: "https://danrleywillyan-blog.firebaseapp.com",
+        title: "linkedin icon/link",
+        icon: account,
+        alt: "github icon/link",
+        text: "Euzinho"
+    },
+    {
+        href: "https://www.instagram.com/danrleypereira/",
+        title: "instagram icon/link",
+        icon: glasses,
+        alt: "github icon/link",
+        text: "Habilidades"
+    },
+    {
+        href: "https://www.facebook.com/danrleywillyan",
+        title: "facebook icon/link",
+        icon: briefcase,
+        alt: "github icon/link",
+        text: "Projetos"
+    },
+    {
+        href: "https://www.facebook.com/danrleywillyan",
+        title: "facebook icon/link",
+        icon: telephone,
+        alt: "github icon/link",
+        text: "Contato"
+    },
+    {
+        href: "https://drive.google.com/file/d/1igtaG0Y6VysFgfO8Mw80EIuDYMKrw8ua/view?usp=sharing",
+        title: "resume link",
+        icon: resume,
+        alt: "download de currículo",
+        text: "Curriculum"
+    }
+]
+
+const socialMidias = [
+    {
+        link: "https://github.com/danrleywillyan",
+        name: "github icon/link",
+        icon: github,
+        spin: true
+    },
+    {
+        link: "https://www.linkedin.com/in/danrley-willyan/",
+        name: "linkedin icon/link",
+        icon: linkedin,
+        spin: false
+    },
+    {
+        link: "https://www.instagram.com/danrleypereira",
+        name: "instagram icon/link",
+        icon: instagram,
+        spin: false
+    },
+    {
+        link: "https://www.facebook.com/danrleywillyan",
+        name: "facebook icon/link",
+        icon: facebook,
+        spin: true
+    }
+]
+
 function AsideLinks() {
-    return(
+    return (
         <div className="links-navigation">
-            <a
-                href="https://github.com/danrleywillyan"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="home link"
-                className="nav-profile"
-            >
-                <div>
-                    <img src={perfil} className="" alt="github icon/link" />
-                    <h3>Danrley Pereira</h3>
-                </div>
-            </a>
-            <a
-                href="https://danrleywillyan-blog.firebaseapp.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="linkedin icon/link"
-            >
-                <div className="nav-link">
-                    <img src={account} className="" alt="github icon/link" />
-                    <h3>Euzinho</h3>
-                </div>
-            </a>
-            <a
-                href="https://www.instagram.com/danrleypereira/"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="instagram icon/link"
-            >
-                <div className="nav-link">
-                    <img src={glasses} className="" alt="github icon/link" />
-                    <h3>Habilidades</h3>
-                </div>
-            </a>
-            <a
-                href="https://www.facebook.com/danrleywillyan"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="facebook icon/link"
-            >
-                <div className="nav-link">
-                    <img src={briefcase} className="" alt="github icon/link" />
-                    <h3>Projetos</h3>
-                </div>
-            </a>
-            <a
-                href="https://www.facebook.com/danrleywillyan"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="facebook icon/link"
-            >
-                <div className="nav-link">
-                    <img src={telephone} className="" alt="github icon/link" />
-                    <h3>Contato</h3>
-                </div>
-            </a>
-            <a
-                href="https://www.facebook.com/danrleywillyan"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="facebook icon/link"
-            >
-                <div className="nav-link">
-                    <img src={resume} className="" alt="github icon/link" />
-                    <h3>Resumé PDF</h3>
-                </div>
-            </a>
+            {
+                items.map((item, index) => {
+                    return (
+                        <a
+                            href={item.href}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title={item.title}
+                            className={index == 0 ? "nav-profile" : ""}
+                        >
+                            <div className={index == 0 ? "" : "nav-link"}>
+                                <img src={item.icon} className="" alt={item.alt} />
+                                <h3>{item.text}</h3>
+                            </div>
+                        </a>
+                    )
+                })
+            }
             <div className="">
-                <a
+                {
+                    socialMidias.map((midia, index) => {
+                        return (
+                            <a
+                                href={midia.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                title={midia.name}
+                            >
+                                <img
+                                    src={midia.icon}
+                                    className={`social-midia-icon ${midia.spin && 'spin'}`}
+                                    alt={midia.name}
+                                />
+                            </a>
+                        )
+                    })
+                }
+                {/* <a
                     className="block-a"
                     href="https://github.com/danrleywillyan"
                     target="_blank"
@@ -94,34 +135,7 @@ function AsideLinks() {
                     title="github icon/link"
                 >
                     <img src={github} className="social-midia-icon spin" alt="github icon/link" />
-                </a>
-                <a
-                    className="block-a"
-                    href="https://www.linkedin.com/in/danrley-willyan/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="linkedin icon/link"
-                >
-                    <img src={linkedin} className="social-midia-icon" alt="linkedin icon/link" />
-                </a>
-                <a
-                    className="block-a"
-                    href="https://www.instagram.com/danrleypereira/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="instagram icon/link"
-                >
-                    <img src={instagram} className="social-midia-icon" alt="instagram icon/link" />
-                </a>
-                <a
-                    className="block-a"
-                    href="https://www.facebook.com/danrleywillyan"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="facebook icon/link"
-                >
-                    <img src={facebook} className="social-midia-icon spin" alt="facebook icon/link" />
-                </a>
+                </a>*/}
             </div>
         </div>
     )
--- src/components/social-midia-aside/aside-social-midia.tsx
+++ src/components/social-midia-aside/aside-social-midia.tsx
@@ -1,45 +1,63 @@
 import React from 'react';
-import facebook from '../../assets/social-midias/facebook-48.svg';
-import linkedin from '../../assets/social-midias/linkedin-48.svg';
-import github from '../../assets/social-midias/github-48.svg';
-import instagram from '../../assets/social-midias/instagram-48.svg';
+// import facebook from '../../assets/social-midias/facebook-48.svg';
+// import linkedin from '../../assets/social-midias/linkedin-48.svg';
+// import github from '../../assets/social-midias/github-48.svg';
+// import instagram from '../../assets/social-midias/instagram-48.svg';
+import facebook from '../../assets/social-midias/facebook.svg';
+import linkedin from '../../assets/social-midias/linkedin.svg';
+import github from '../../assets/social-midias/github.svg';
+import instagram from '../../assets/social-midias/instagram.svg';
 import './aside-social-midia.css';
+
+const socialMidias = [
+  {
+    link: "https://github.com/danrleywillyan",
+    name: "github icon/link",
+    icon: github,
+    spin: true
+  },
+  {
+    link: "https://www.linkedin.com/in/danrleypereira/",
+    name: "linkedin icon/link",
+    icon: linkedin,
+    spin: false
+  },
+  {
+    link: "https://www.instagram.com/danrleypereira",
+    name: "instagram icon/link",
+    icon: instagram,
+    spin: false
+  },
+  {
+    link: "https://www.facebook.com/danrleywillyan",
+    name: "facebook icon/link",
+    icon: facebook,
+    spin: true
+  }
+]
+
 
 function SocialMidiaAside() {
   return (
     <div className="social-midia-aside">
-        <a
-          href="https://github.com/danrleywillyan"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="github icon/link"
-        >
-          <img src={github} className="social-midia-icon spin" alt="github icon/link" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/danrley-willyan/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="linkedin icon/link"
-        >
-            <img src={linkedin} className="social-midia-icon" alt="linkedin icon/link" />
-        </a>
-        <a
-          href="https://www.instagram.com/danrleypereira/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="instagram icon/link"
-        >
-            <img src={instagram} className="social-midia-icon" alt="instagram icon/link" />
-        </a>
-        <a
-          href="https://www.facebook.com/danrleywillyan"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="facebook icon/link"
-        >
-            <img src={facebook} className="social-midia-icon spin" alt="facebook icon/link" />
-        </a>
+      {
+        socialMidias.map((midia, index) => {
+          return (
+            <a
+              href={midia.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              title={midia.name}
+            >
+              <img
+                src={midia.icon}
+                className={`social-midia-icon ${midia.spin && 'spin'}`}
+                alt={midia.name}
+              />
+            </a>
+          )
+        })
+      }
     </div>
   );
 }
